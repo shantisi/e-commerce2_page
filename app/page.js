41fd48f6a120801732933979f5fe37ba4af546ca
shanti singh <shantisingh22@navgurukul.org>
2023-12-05 20:11:11 +0530

    subfooter

--- app/page.js
+++ app/page.js
@@ -3,12 +3,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import Header from "./Header.js"
 import 	Sliderbox from "./Sliderbox";
 import Home from "./Homepage";
+import Subfooter from "./subfooter";
 const Page = () => {
   return (
     <>
       <Header/><br/>
       <Sliderbox/><br/>
-       <Home/><br/>
+      <Home/><br/>
+      <Subfooter/><br/>
+       
     </>
   );
 };
---
+++ app/subfooter.js
@@ -0,0 +1,31 @@
+import React from 'react';
+const Subfooter = () => {
+  return( 
+    <>  
+       <div className="container-fulid">
+           <div className="header" style={{ backgroundColor: "lightgray"}}>
+               <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png" alt="logo image" class="img-fuild mx-2 mt-2" style={{height:"50px"}}/>
+                    </li>
+                    <li class="nav-item">
+                        <img src="https://cdn.pixabay.com/photo/2021/06/15/12/14/instagram-6338392_1280.png" alt="logo image" class="img-fuild mx-2 mt-2" style={{height:"50px"}}/>
+                    </li>
+                    <li class="nav-item">
+                        <img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt="logo image"class="img-fulid mx-2"  style={{height:"65px"}}/>
+                    </li>
+                    <li class="nav-item">
+                        <img src="https://freelogopng.com/images/all_img/1656501968youtube-icon-png.png" alt="logo image" class="img-fuild mx-2 mt-2" style={{height:"50px"}}/>
+                    </li>
+                    <li class="nav-item">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png" alt="logo image" class="img-fuild mx-2 mt-2" style={{height:"50px"}}/>
+                    </li>
+                </ul>            
+              
+           </div>
+       </div>      
+    </>    
+  );
+};
+
+export default Subfooter;
