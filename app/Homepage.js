2edce070de5e0b300f6dd2cd5544db99c89c0bbb
shanti singh <shantisingh22@navgurukul.org>
2023-12-05 19:59:39 +0530

    Homepage

---
+++ app/Homepage.js
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <>
+      <div className="home" style={{ backgroundColor: "lightgray" }}>
+        <div className="row mx-4" style={{ width: "100%" }}>
+          <div className="col-2 mx-2 border" style={{ height: "300px" }}>box1</div>
+          <div className="col-2 mx-2 border" style={{ height: "300px" }}>box2</div>
+          <div className="col-2 mx-2 border" style={{ height: "300px" }}>box3</div>
+          <div className="col-2 mx-2 border" style={{ height: "300px" }}>box4</div>
+          <div className="col-2 mx-2 border" style={{ height: "300px" }}>box4</div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import Link from "next/link";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "./Header.js"
 import 	Sliderbox from "./Sliderbox";
+import Home from "./Homepage";
 const Page = () => {
   return (
     <>
       <Header/><br/>
       <Sliderbox/><br/>
+       <Home/><br/>
     </>
   );
 };
