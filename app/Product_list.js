c7371cd5343339703bfdf56736d9b96a2d959645
shanti singh <shantisingh22@navgurukul.org>
2023-12-17 20:03:00 +0530

    product_list page

---
+++ app/Product_list.js
@@ -0,0 +1,250 @@
+
+import React from 'react';
+
+const ProductList = () => {
+  return (
+    <>
+      <div className="container-fluid mt-4">
+        <div className="row">
+          <div className="col-sm-2 mt-2 mx-auto">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title" style={{ fontWeight: '1000' }}>CLOTHS CATEGORY</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-0">
+            <div className="col-sm-12 text-right">
+                <a href="/listing" className="custom-left-align">
+                <div className="Men">Men's Clothes</div>
+                <h5 className="card-title custom-left-align-text">View All</h5>
+                </a>
+            </div>
+        </div>
+
+       <div className="row">
+          <div className="col-sm-2 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <img src="https://www.badmaash.com/cdn/shop/products/0W2A7195_ac9e8ee8-3afe-4341-89be-26071c80edcf_2048x2048.jpg?v=1665469549" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}} />
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-2 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <img src="https://i.pinimg.com/originals/85/22/34/8522346c05525356198706df30c7ebe0.png" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-2 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <img src="https://images.unsplash.com/photo-1617137968427-85924c800a22?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWVucyUyMGZhc2hpb258ZW58MHx8MHx8fDA%3D" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-2 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <img src="https://rukminim2.flixcart.com/image/832/832/xif0q/jacket/u/j/5/l-1-no-multicolor-part-3-azmani-original-imaguubsvarhyb4n.jpeg?q=70" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}style={{width:"100%",height:"316px"}}/>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-2 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <img src="https://c4.wallpaperflare.com/wallpaper/110/387/806/movies-bollywood-movies-wallpaper-preview.jpg" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-2 mb-3">
+            <div className="card">
+              <div className="card-body">
+                <img src="https://i.pinimg.com/736x/11/23/6a/11236aa7512eec2ef55c96f45031d4ba.jpg" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+      <a href="/listing" className="custom-left-align">
+        <div className="Women">Woen's Clothes</div>
+        <h5 className="card-title custom-left-align-text">View All</h5>
+      </a>
+
+      <div className="row">
+        <div className="col-sm-2 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRR4_ZhztP0SvApVMUU7_o8X44w0Zb041YNA&usqp=CAU&reload=on" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://assets.ajio.com/medias/sys_master/root/20231122/FYfs/655d30fdafa4cf41f597b086/-473Wx593H-466817701-beige-MODEL.jpg" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://lmsin.net/cdn-cgi/image/h=831,w=615,q=85,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max-new/1000012060746-Orange-RUSTCORAL-1000012060746_01-2100.jpg" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfb-pzQU7FShVqlM34F-ot0EQp-43eUUs3pw&usqp=CAU" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQ-zkH3RttjVBLrjmutAMEC_GUkqk-MEkci6rSn0SH6Gq-Ddz_KKZK5Fbgvito8nTjR7JhWOuBT-nLg8qHGCWNVAIfRjsnHuJLLA4PKFLY" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-3">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://assets.ajio.com/medias/sys_master/root/20231031/0B3h/65402de9afa4cf41f569c18e/-1117Wx1400H-466757692-green-MODEL.jpg" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+      
+      <a href="/listing" className="custom-left-align">
+        <div className="Jewelery">Jewelery</div>
+        <h5 className="card-title custom-left-align-text">View All</h5>
+      </a>
+
+      <div className="row"mt-4 >
+        <div className="col-sm-2 mb-4">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVQMrH4sF1J1MdR86JTNVzBmIc8aCGxFdAjQ&usqp=CAU" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-4">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1Pv1SWdIy9bgakEC-b7YSbkkTq8oM5FGQFQ&usqp=CAU" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-4">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0VpJmZ6GS42peT9d7XI9Z-uyDBCKQpqqPzw&usqp=CAU" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-4">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ25-arzBcxD2kuxf4fASw_v2zKdDX_uQrP2w&usqp=CAU" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-4">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpTy4PdSgj7F7Td_CZby3uxRj8VLSJw7lk6Q&usqp=CAU" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2 mb-4">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsiJtUkv-aUfscMiYlHMe2tKzroXSFhz090w&usqp=CAU" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+      
+      <a href="/listing" className="custom-left-align">
+        <div className="Electronics">Electronics</div>
+        <h5 className="card-title custom-left-align-text">View All</h5>
+      </a>
+
+      <div className="row" mt-4>
+        <div className="col-sm-2">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwOs3oswyG4dG_cqFYlrlKqYmDyjtWxcoNe8MLh_JykSizpvuX-jjpQ-HTXIhd2pzXLbs&usqp=CAU" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDsvaWip63hqRTQpR6_RGJxtib5jo1fXkzHD-wdesTLAWy-vbXFRQyG8YIP-HMheqDuHc&usqp=CAU" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIrz6WSVY0xGIziQ_B5qg_XO0qdsxrtEfWm_6IlSI8iVIhHet0z01yCTYhfQjgxBrIZh4&usqp=CAU" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrfUXGRLZI37xwZcPfNJzKZfNKBN5oX8BIUQ&usqp=CAU" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkvRvUeWUxrn0ueyphmPfbzWOITtY_PNinSw&usqp=CAU" alt="Left Box Image" className="img-fluid" style={{width:"100%",height:"316px"}}/>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-2">
+          <div className="card">
+            <div className="card-body">
+              <img src="https://storage.googleapis.com/pickaboo-prod/media/catalog/product/cache/90e3b9f4120fc209bf60003e3b0e1323/i/n/infinix-smart-8-timber-black-6.jpg" alt="Left Box Image" className="img-fluid"style={{width:"100%",height:"316px"}} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "./Header.js"
 import Sliderbox from "./Sliderbox";
-import Home from "./Homepage";
+import Home from "./Product_list";
 import Subfooter from "./subfooter";
 import Footer from "./Footer.js";
 const Page = () => {
